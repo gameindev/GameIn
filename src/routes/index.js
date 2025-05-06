72d97e8329436de1ef28ed7e352fc05277a90132
grvroy6490 <roy.gourav6490@gmail.com>
2025-05-06 13:26:39 +0530

    Initial backend commit

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
