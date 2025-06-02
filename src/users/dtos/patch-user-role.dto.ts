6c4c0bd973ef2780db9066a68c5d5e27dbb1d8fd
grvroy6490 <roy.gourav6490@gmail.com>
2025-06-03 00:27:25 +0530

    updated /assign-role api to receive password and usertype as parameters

--- src/users/dtos/patch-user-role.dto.ts
+++ src/users/dtos/patch-user-role.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsInt, IsNotEmpty } from "class-validator";
+import { IsEnum, IsInt, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 import { UserType } from "../enums/user-type.enums";
 import { ApiProperty } from "@nestjs/swagger";
 
@@ -14,4 +14,22 @@ export class PathcUserRoleDto {
     @IsEnum(UserType)
     @IsNotEmpty()
     userType: UserType;
+
+
+    @ApiProperty({
+        description: 'The password of the user',
+        type: String,
+        example: 'Kj#9mP$2nL',
+        minLength: 8,
+        maxLength: 96,
+    })
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(96)
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+        message:
+            'Minimum eight characters, at least one letter, one number and one special character',
+    })
+    password: string;
 }
--- src/users/providers/update-user-role.provider.ts
+++ src/users/providers/update-user-role.provider.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, forwardRef, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user.entity';
 import { Repository } from 'typeorm';
 import { PathcUserRoleDto } from '../dtos/patch-user-role.dto';
 import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
+import { HashingProvider } from 'src/auth/providers/hashing.provider';
 
 @Injectable()
 export class UpdateUserRoleProvider {
@@ -11,11 +12,20 @@ export class UpdateUserRoleProvider {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
+
+        /**
+         * Injecting HashingProvider.
+         */
+        @Inject(forwardRef(() => HashingProvider))
+        private readonly hashingProvider: HashingProvider,
     ) { }
 
 
     async updateOAuthUserRole(patchUserRoleDto: PathcUserRoleDto, userSub: ActiveUserData) {
+        const { password, userType } = patchUserRoleDto;
         let user = undefined;
+
+        const hashedPassword = await this.hashingProvider.hashPassword(password);
 
         try {
             user = await this.userRepository.findOneBy({ id: userSub.sub  });
@@ -28,12 +38,24 @@ export class UpdateUserRoleProvider {
         }
 
         try {
-            user.userType = patchUserRoleDto.userType;
+            user.userType = userType;
+            user.password = hashedPassword;
             await this.userRepository.save(user);
         } catch (error) {
             throw new InternalServerErrorException('Something went wrong while updating user');
         }
 
-        return user;
+        return {
+            "username": user.username,
+            "email": user.email,
+            "userType": user.userType,
+            "dateOfBirth": user.dateOfBirth,
+            "isActive": user.isActive,
+            "isVerified": user.isVerified,
+            "isFirst": user.isFirst,
+            "createdAt": user.createdAt,
+            "updatedAt": user.updatedAt,
+            "deletedAt": user.deletedAt
+          }
     }
 }
