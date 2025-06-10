645860f726246441440d5b9fc4254b13f5a4b49f
gameindev <gameindev@users.noreply.github.com>
2025-06-10 11:53:19 +0000

    deploy: 626013a84a0f47bb8bd9ca3e8785d6cd0165df18

--- assets/star-pVRGcptN.js
+++ assets/star-pVRGcptN.js
@@ -1,4 +1,4 @@
-import{c as a}from"./createLucideIcon-7bouigEo.js";/**
+import{c as a}from"./createLucideIcon-Bdz_0BOg.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
