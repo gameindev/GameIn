82b842df64b3a06c36d416122dbe8fbaefef3408
grvroy6490 <roy.gourav6490@gmail.com>
2025-06-10 17:54:40 +0530

    updated route endpoints

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -1,14 +1,14 @@
 const routePaths = {
-  welcomePage: "/",
-  login: "/login",
-  register: "/register",
-  dashboard: "/dashboard",
-  profile: "/profile",
-  sponsorships: "/sponsorships",
-  offerings: "/offerings",
-  stats: "/stats",
-  newsfeed: "/newsfeed",
-  inbox: "/inbox",
+  welcomePage: "GameIn/",
+  login: "GameIn/login",
+  register: "GameIn/register",
+  dashboard: "GameIn/dashboard",
+  profile: "GameIn/profile",
+  sponsorships: "GameIn/sponsorships",
+  offerings: "GameIn/offerings",
+  stats: "GameIn/stats",
+  newsfeed: "GameIn/newsfeed",
+  inbox: "GameIn/inbox",
 };
 
 export default routePaths;
