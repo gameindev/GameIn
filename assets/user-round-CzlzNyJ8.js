322f5bdd1b5ed00a1e1f2c3cfe293868e8ce2335
gameindev <gameindev@users.noreply.github.com>
2025-06-10 11:50:13 +0000

    deploy: be6d40193cac48bcd54f6644adf3e5aed443990d

---
+++ assets/user-round-CzlzNyJ8.js
@@ -0,0 +1,6 @@
+import{c}from"./createLucideIcon-7bouigEo.js";function n(o,e){return typeof o=="boolean"?o:e.autoContrast}/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["circle",{cx:"12",cy:"8",r:"5",key:"1hypcn"}],["path",{d:"M20 21a8 8 0 0 0-16 0",key:"rfgkzh"}]],a=c("user-round",r);export{a as U,n as g};
