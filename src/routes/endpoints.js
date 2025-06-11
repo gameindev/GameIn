459a7d7832c7ba196218acfb1591781473549452
grvroy6490 <roy.gourav6490@gmail.com>
2025-06-11 13:52:11 +0530

    testing update

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -1,14 +1,14 @@
 const routePaths = {
-  welcomePage: "GameIn/",
-  login: "GameIn/login",
-  register: "GameIn/register",
-  dashboard: "GameIn/dashboard",
-  profile: "GameIn/profile",
-  sponsorships: "GameIn/sponsorships",
-  offerings: "GameIn/offerings",
-  stats: "GameIn/stats",
-  newsfeed: "GameIn/newsfeed",
-  inbox: "GameIn/inbox",
+  welcomePage: "/",
+  login: "/login",
+  register: "/register",
+  dashboard: "/dashboard",
+  profile: "/profile",
+  sponsorships: "/sponsorships",
+  offerings: "/offerings",
+  stats: "/stats",
+  newsfeed: "/newsfeed",
+  inbox: "/inbox",
 };
 
 export default routePaths;
