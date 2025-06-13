f6c94d9ae41a394b33bed5fcae23fb0c20691863
sathish-ui-dev <sathish-ui-dev@users.noreply.github.com>
2025-06-13 19:45:34 +0000

    deploy: af1255fdf2cf79fc4e1df7b3f76c267b29c59293

---
+++ assets/RequiresAuth-DNwK-zua.js
@@ -0,0 +1,1 @@
+import{l as t,j as e,O as a,ab as o,m as r}from"./index-BGIsBczW.js";const n=()=>t(s=>s.auth.accessToken)?e.jsx(a,{}):e.jsx(o,{to:r.LOGIN,replace:!0});export{n as default};
---
+++ assets/selectors-BSredJDX.js
@@ -0,0 +1,1 @@
+const c=s=>{var e;return!!((e=s.auth)!=null&&e.accessToken)},o=s=>s.user.profile;export{o as c,c as i};
