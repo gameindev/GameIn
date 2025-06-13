7e0ad6a27c618e26dc3745d9e4b5042c7a30e57b
sandy <sathishsandy8124@gmail.com>
2025-06-14 01:07:28 +0530

    oauth update with custom hook

--- src/components/shared/oAuth/CompleteProfile.jsx
+++ src/components/shared/oAuth/CompleteProfile.jsx
@@ -1,27 +1,19 @@
-import {
-  Modal,
-  Button,
-  TextInput,
-  Select,
-  Paper,
-  Stack,
-  Title,
-} from "@mantine/core";
-import { useSelector } from "react-redux";
+import { Modal, Button, TextInput, Select, Paper, Stack, Title, } from "@mantine/core";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 import { OAuthProfile } from "../../../utils/validationSchema";
 import { USERTYPES } from "./../../../utils/enum";
 import FormField from './../ui/FormField';
+import useApi from "../../../hooks/useApi";
+import { API_PATHS } from "../../../services/endpoints";
 
 const defaultValues = {
   userType: "",
   password: "",
 };
 
-export default function CompleteProfile({ opened, onComplete }) {
-  const accessToken = useSelector((state) => state.auth.accessToken);
+export default function CompleteProfile({ opened, onComplete, accessToken }) {
 
   const { control, handleSubmit } = useForm({
     defaultValues,
@@ -29,28 +21,25 @@ export default function CompleteProfile({ opened, onComplete }) {
     mode: "onSubmit",
   });
 
-  const onSubmit = async (formData) => {
-    try {
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/users/assign-role`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify(formData),
-      });
+  const { patch } = useApi();
 
-      const { data } = await res.json();
+  const onSubmit = async (formData) => {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    };
+    try {
+      const { data } = await patch(API_PATHS.AUTH.SELECT_ROLE, formData, headers);
       onComplete({ user: data });
     } catch (error) {
-      console.error("Complete profile error:", error);
+      return Promise.reject("Complete profile error:", error);
     }
   };
 
   return (
     <Modal
       opened={opened}
-      onClose={() => {}}
+      onClose={() => { }}
       withCloseButton={false}
       title="Please Update Your Credentials"
       centered
