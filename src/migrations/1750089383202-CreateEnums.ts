370866fee1f00121985f2d80aa6760874e9d528c
grvroy6490 <roy.gourav6490@gmail.com>
2025-06-27 03:36:53 +0530

    updated enum migrations

--- src/migrations/1750089383202-CreateEnums.ts
+++ src/migrations/1750089383202-CreateEnums.ts
@@ -4,16 +4,28 @@ export class CreateEnums1750089383202 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE TYPE "user_usertype_enum" AS ENUM ('ADMIN', 'CREATOR', 'BRAND', 'COMMUNITY');
-          `);
+      DO $$ BEGIN
+        IF NOT EXISTS (
+          SELECT 1 FROM pg_type WHERE typname = 'user_usertype_enum'
+        ) THEN
+          CREATE TYPE "user_usertype_enum" AS ENUM ('ADMIN', 'CREATOR', 'BRAND', 'COMMUNITY');
+        END IF;
+      END $$;
+    `);
+
         await queryRunner.query(`
-            CREATE TYPE "upload_type_enum" AS ENUM ('image', 'video');
-          `);
+      DO $$ BEGIN
+        IF NOT EXISTS (
+          SELECT 1 FROM pg_type WHERE typname = 'upload_type_enum'
+        ) THEN
+          CREATE TYPE "upload_type_enum" AS ENUM ('image', 'video');
+        END IF;
+      END $$;
+    `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TYPE "upload_type_enum";`);
-        await queryRunner.query(`DROP TYPE "user_usertype_enum";`);
+        await queryRunner.query(`DROP TYPE IF EXISTS "upload_type_enum";`);
+        await queryRunner.query(`DROP TYPE IF EXISTS "user_usertype_enum";`);
     }
-
 }
