67a70517d82d85f04ae91c27bb9b1ead740d2f88
grvroy6490 <roy.gourav6490@gmail.com>
2025-09-21 22:52:07 +0530

    Merged conflict

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -1,77 +1,61 @@
 const routePaths = {
-    WELCOMEPAGE: "/",
-    LOGIN: "/login",
-    REGISTER: "/register",
-    SEARCH: "/search/:userType",
-    ACCOUNTS: {
-        DASHBOARD: {
-            ROOT: "/dashboard",
-        },
-        PROFILE: {
-            ROOT: "/profile",
-            BIO: "/profile/bio",
-            FAQ: "/profile/faq",
-            CREATE_TEAM: "/profile/create-team",
-            VIEW: "/profile/:id",
-        },
-        SPONSORSHIPS: {
-            ROOT: "/sponsorships",
-            VIEW: "/sponsorships/:id",
-        },
-        OFFERINGS: {
-            ROOT: "/offerings",
-            CREATE_OFFERING: "/offerings/create-offering",
-            EDIT_OFFERING: "/offerings/edit-offering",
-            EDIT_PRICEPOOLEVENT: "/offerings/edit-pricepool-event",
-            LIST_OFFERINGS: "/profile/:id/offerings",
-        },
-        STATS: {
-            ROOT: "/stats",
-        },
-        NEWSFEED: {
-            ROOT: "/newsfeed",
-        },
-        INBOX: {
-            ROOT: "/inbox",
-            VIEW: "/inbox/:id",
-        },
+  WELCOMEPAGE: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  SEARCH: "/search/:userType",
+  ACCOUNTS: {
+    DASHBOARD: {
+      ROOT: "/dashboard",
     },
-    SETTINGS: {
-        ROOT: "/settings",
-        ACCOUNT: "/settings/account",
-        CONTACT: "/settings/contact",
-        PRIVACY: "/settings/privacy",
+    PROFILE: {
+      ROOT: "/profile",
+      BIO: "/profile/bio",
+      FAQ: "/profile/faq",
+      CREATE_TEAM: "/profile/create-team",
+      VIEW: "/profile/:id",
     },
-    FOOTER: {
-        ROOT: "/footer",
-        ABOUT: "/footer/about",
-        INFO: "/footer/info",
-        TERMS: "/footer/terms",
+    SPONSORSHIPS: {
+      ROOT: "/sponsorships",
+      VIEW: "/sponsorships/:id",
     },
     OFFERINGS: {
-        ROOT: "/offerings",
-        CREATE_OFFERING: "/offerings/create-offering",
-        EDIT_OFFERING: "/offerings/edit-offering",
-        EDIT_PRICEPOOLEVENT: "/offerings/edit-pricepool-event",
-        VIEW: "/offerings/:id",
+      ROOT: "/offerings",
+      CREATE_OFFERING: "/offerings/create-offering",
+      EDIT_OFFERING: "/offerings/edit-offering",
+      EDIT_PRICEPOOLEVENT: "/offerings/edit-pricepool-event",
+      VIEW: "/offerings/:id",
     },
     STATS: {
-        ROOT: "/stats",
+      ROOT: "/stats",
     },
     NEWSFEED: {
-        ROOT: "/newsfeed",
+      ROOT: "/newsfeed",
     },
     INBOX: {
-        ROOT: "/inbox",
-        VIEW: "/inbox/:id",
+      ROOT: "/inbox",
+      VIEW: "/inbox/:id",
     },
-    helpers: {
-        profile: (id) => `/profile/${id}`,
-        sponsorship: (id) => `/sponsorships/${id}`,
-        offering: (id) => `/offerings/${id}`,
-        inbox: (id) => `/inbox/${id}`,
-        profileTab: (id, tab) => `/profile/${id}/${tab}`,
-    },
+  },
+  SETTINGS: {
+    ROOT: "/settings",
+    ACCOUNT: "/settings/account",
+    CONTACT: "/settings/contact",
+    PRIVACY: "/settings/privacy",
+  },
+  FOOTER: {
+    ROOT: "/footer",
+    ABOUT: "/footer/about",
+    INFO: "/footer/info",
+    TERMS: "/footer/terms",
+  },
+
+  helpers: {
+    profile: (id) => `/profile/${id}`,
+    sponsorship: (id) => `/sponsorships/${id}`,
+    offering: (id) => `/offerings/${id}`,
+    inbox: (id) => `/inbox/${id}`,
+    profileTab: (id, tab) => `/profile/${id}/${tab}`,
+  },
 };
 
 export default routePaths;
